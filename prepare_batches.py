81ee7523090d02577d3aa0de6d32bed4b61705d9
Dartrisen <calculating.scientist@gmail.com>
2024-05-04 14:53:59 -0700

    network class have been added

--- prepare_batches.py
+++ prepare_batches.py
@@ -5,6 +5,20 @@ import numpy as np
 from PIL import Image
 
 
+label_dict = {
+    0: 'airplane',
+    1: 'automobile',
+    2: 'bird',
+    3: 'cat',
+    4: 'deer',
+    5: 'dog',
+    6: 'frog',
+    7: 'horse',
+    8: 'ship',
+    9: 'truck'
+}
+
+
 def unpickle(file):
     with open(file, 'rb') as fo:
         data = pickle.load(fo, encoding='bytes')
@@ -12,7 +26,7 @@ def unpickle(file):
 
 
 # noinspection PyTypeChecker
-def save_images(data: str, directory: str):
+def save_images(data: str, directory: str) -> None:
     num_images = len(data[b'data'])
 
     if not os.path.exists(directory):
@@ -26,12 +40,12 @@ def save_images(data: str, directory: str):
         image_label = data[b'labels'][i]
 
         # Create subdirectory for each label if it doesn't exist
-        label_dir = os.path.join(directory, f'category_{image_label}')
+        label_dir = os.path.join(directory, f'{label_dict[image_label]}')
         if not os.path.exists(label_dir):
             os.makedirs(label_dir)
 
         # Save image with appropriate filename
-        image_filename = f'image_{i}__label_{image_label}.jpg'
+        image_filename = f'image_{i}__label_{image_label}.png'
         image_path = os.path.join(label_dir, image_filename)
         image = Image.fromarray(image_data)
         image.save(image_path)
@@ -39,7 +53,7 @@ def save_images(data: str, directory: str):
 
 
 def main():
-    path = "path/to/dir"
+    path = "/Users/dartrisen/Downloads/cifar-10-batches-py/data_batch_1"
     data_batch = unpickle(path)
     output_dir = 'train'
     save_images(data_batch, output_dir)
