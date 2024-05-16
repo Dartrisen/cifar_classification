1b79c35d370843587a33cfd8b66770f3fd04f4db
Dartrisen <calculating.scientist@gmail.com>
2024-05-16 00:25:18 -0700

    some improvements, model.py has been added

--- main.py
+++ main.py
@@ -4,16 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torchvision
-from torch.autograd import Variable
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 
-from prepare_data import CustomDataset, transformations
 from model import Network
+from prepare_data import CustomDataset, transformations
 
-batch_size = 3
+batch_size = 100
 number_of_labels = 3
 
 # Create train and test datasets
@@ -41,7 +39,7 @@ optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
 
 # Function to save the model
 def save_model():
-    path = "./myFirstModel.pth"
+    path = "./model.pth"
     torch.save(model.state_dict(), path)
 
 
@@ -54,14 +52,12 @@ def test_accuracy():
     with torch.no_grad():
         for data in test_loader:
             images, labels = data
-            # run the model on the test set to predict labels
+            images, labels = images.to(device), labels.to(device)
             outputs = model(images)
-            # the label with the highest energy will be our prediction
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             accuracy += (predicted == labels).sum().item()
 
-    # compute the accuracy over all test images
     accuracy = (100 * accuracy / total)
     return accuracy
 
@@ -70,48 +66,30 @@ def test_accuracy():
 def train(num_epochs):
     best_accuracy = 0.0
 
-    # Define your execution device
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # mps
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
     print("The model will be running on", device, "device")
-    # Convert model parameters and buffers to CPU or Cuda
     model.to(device)
 
-    for epoch in range(num_epochs):  # loop over the dataset multiple times
+    for epoch in range(num_epochs):
         running_loss = 0.0
-        running_acc = 0.0
 
         for i, (images, labels) in enumerate(train_loader, 0):
-            # get the inputs
-            images = Variable(images)
-            # Convert labels to tensor before applying .to(device)
-            labels = Variable(labels)
-            # print("label size: ", labels.shape)
-            # labels = Variable(labels.to(device))
+            images, labels = images.to(device), labels.to(device)
 
-            # zero the parameter gradients
             optimizer.zero_grad()
-            # predict classes using images from the training set
             outputs = model(images)
-            # compute the loss based on model output and real labels
             loss = loss_fn(outputs, labels)
-            # backpropagate the loss
             loss.backward()
-            # adjust parameters based on the calculated gradients
             optimizer.step()
 
-            # Let's print statistics for every 1,000 images
-            running_loss += loss.item()  # extract the loss value
+            running_loss += loss.item()
             if i % 1000 == 999:
-                # print every 1000 (twice per epoch)
                 print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 1000))
-                # zero the loss
                 running_loss = 0.0
 
-        # Compute and print the average accuracy fo this epoch when tested over all 10000 test images
         accuracy = test_accuracy()
         print('For epoch', epoch + 1, 'the test accuracy over the whole test set is %d %%' % (accuracy))
 
-        # we want to save the model if the accuracy is the best
         if accuracy > best_accuracy:
             save_model()
             best_accuracy = accuracy
@@ -119,46 +97,35 @@ def train(num_epochs):
 
 # Function to show the images
 def image_show(img):
-    img = img / 2 + 0.5  # unnormalize
-    npimg = img.numpy()
+    img = img / 2 + 0.5
+    npimg = img.cpu().numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
 
 # Function to test the model with a batch of images and show the labels predictions
 def test_batch():
-    # get batch of images from the test DataLoader
     images, labels = next(iter(test_loader))
-
-    # show all images as one image grid
+    images, labels = images.to(device), labels.to(device)
     image_show(torchvision.utils.make_grid(images))
-
-    # Show the real labels on the screen
     print('Real labels: ', ' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
-
-    # Let's see what if the model identifiers the  labels of those example
     outputs = model(images)
-
-    # We got the probability for every 10 labels. The highest (max) probability should be correct label
     _, predicted = torch.max(outputs, 1)
-
-    # Let's show the predicted labels on the screen to compare with the real ones
     print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(batch_size)))
 
 
 if __name__ == "__main__":
-    # Let's build our model
-    train(50)
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    model.to(device)
+
+    train(1000)
     print('Finished Training')
 
-    # Test which classes performed well
     accuracy = test_accuracy()
     print(accuracy)
 
-    # Let's load the model we just created and test the accuracy per label
     model = Network(number_of_labels)
-    path = "myFirstModel.pth"
-    model.load_state_dict(torch.load(path))
+    model.load_state_dict(torch.load("model.pth"))
+    model.to(device)
 
-    # Test with batch of images
     test_batch()
--- model.py
+++ model.py
@@ -1,29 +1,32 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 # Define a convolution neural network
 class Network(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes: int):
         super(Network, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=5, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(12)
-        self.conv2 = nn.Conv2d(in_channels=12, out_channels=12, kernel_size=5, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(12)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv4 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=5, stride=1, padding=1)
-        self.bn4 = nn.BatchNorm2d(24)
-        self.conv5 = nn.Conv2d(in_channels=24, out_channels=24, kernel_size=5, stride=1, padding=1)
-        self.bn5 = nn.BatchNorm2d(24)
-        self.fc1 = nn.Linear(24 * 10 * 10, num_classes)  # Adjusting input size to match resized images
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=12, kernel_size=5, stride=1, padding=1),
+            nn.BatchNorm2d(12),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=12, out_channels=12, kernel_size=5, stride=1, padding=1),
+            nn.BatchNorm2d(12),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(in_channels=12, out_channels=24, kernel_size=5, stride=1, padding=1),
+            nn.BatchNorm2d(24),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=24, out_channels=24, kernel_size=5, stride=1, padding=1),
+            nn.BatchNorm2d(24),
+            nn.ReLU(inplace=True)
+        )
 
-    def forward(self, input):
-        output = F.relu(self.bn1(self.conv1(input)))
-        output = F.relu(self.bn2(self.conv2(output)))
-        output = self.pool(output)
-        output = F.relu(self.bn4(self.conv4(output)))
-        output = F.relu(self.bn5(self.conv5(output)))
-        # print("output shape: ", output.shape)
-        output = output.view(-1, 24 * 10 * 10)
-        output = self.fc1(output)
-        return output
+        self.classifier = nn.Linear(24 * 10 * 10, num_classes)  # Adjusting input size to match resized images
+
+    def forward(self, x: torch.Tensor):
+        x = self.features(x)
+        x = x.view(-1, 24 * 10 * 10)
+        x = self.classifier(x)
+        return x
