0ebcd90f949b7a8d82fa645cbc668680684e03aa
Dartrisen <calculating.scientist@gmail.com>
2024-05-16 22:56:10 -0700

    test_step func has been added

--- tiny_vgg.py
+++ tiny_vgg.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import torch.nn as nn
+from tqdm.auto import tqdm
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
@@ -14,8 +15,9 @@ device = "mps" if torch.backends.mps.is_available() else "cpu"
 print(f"Using device: {device}")
 
 # set up hyperparameters
-IMAGE_SIZE = (32, 32)
+IMAGE_SIZE = (224, 224)
 BATCH_SIZE = 100
+NUM_EPOCHS = 10
 NUMBER_OF_LABELS = 3
 
 transformations = transforms.Compose([
@@ -37,6 +39,22 @@ print("The number of images in a test set is: ", len(test_loader)*BATCH_SIZE)
 print("The number of batches per epoch is: ", len(train_loader))
 
 
+def print_train_time(start, end, device=None):
+    """Prints difference between start and end time.
+    Args:
+        start (float): Start time of computation (preferred in timeit format).
+        end (float): End time of computation.
+    Returns:
+        float: time between start and end in seconds (higher is longer).
+    """
+    total_time = end - start
+    if device:
+        print(f"\nTrain time using PyTorch device {device}: {total_time:.3f} seconds\n")
+    else:
+        print(f"\nTrain time: {total_time:.3f} seconds\n")
+    return round(total_time, 3)
+
+
 class TinyVGG(nn.Module):
     def __init__(self, input_shape: int, hidden_shape: int, output_shape: int) -> None:
         super(TinyVGG, self).__init__()
@@ -56,7 +74,7 @@ class TinyVGG(nn.Module):
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(in_features=hidden_shape * 5 * 5, out_features=output_shape)  # image shape 32, 32
+            nn.Linear(in_features=hidden_shape * 53 * 53, out_features=output_shape)  # image shape 32, 32
         )
 
     def forward(self, x: torch.Tensor):
@@ -67,7 +85,7 @@ class TinyVGG(nn.Module):
 
 
 model = TinyVGG(input_shape=3, hidden_shape=10, output_shape=3)
-print(model(torch.randn(1, 3, 32, 32)))
+print(model(torch.randn(1, 3, 224, 224)))
 
 
 def train_step(
@@ -101,3 +119,30 @@ def train_step(
     train_loss = train_loss / len(dataloader)
     train_acc = train_acc / len(dataloader)
     return train_loss, train_acc
+
+
+def test_step(
+        model: torch.nn.Module,
+        dataloader: torch.utils.data.DataLoader,
+        loss_fn: torch.nn.Module,
+        device: torch.device
+):
+    model.eval()
+
+    test_loss, test_acc = 0, 0
+
+    with torch.inference_mode():
+        for batch, (x, y) in enumerate(dataloader):
+            x, y = x.to(device, non_blocking=True), y.to(device, non_blocking=True)
+
+            test_pred_logits = model(x)
+
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
+
+            test_pred_labels = test_pred_logits.argmax(dim=1)
+            test_acc += ((test_pred_labels == y).sum().item() / len(test_pred_labels))
+
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
