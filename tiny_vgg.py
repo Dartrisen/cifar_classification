0255902a52e6e4fbe5b803f84ec3acf14f1450a0
Dartrisen <calculating.scientist@gmail.com>
2024-05-16 23:07:53 -0700

    train func has been added

--- tiny_vgg.py
+++ tiny_vgg.py
@@ -7,15 +7,8 @@ from torch.utils.data import DataLoader
 
 from prepare_data import CustomDataset
 
-print(f"PyTorch version: {torch.__version__}")
-print(f"Is MPS (Metal Performance Shader) built? {torch.backends.mps.is_built()}")
-print(f"Is MPS available? {torch.backends.mps.is_available()}")
-
-device = "mps" if torch.backends.mps.is_available() else "cpu"
-print(f"Using device: {device}")
-
 # set up hyperparameters
-IMAGE_SIZE = (224, 224)
+IMAGE_SIZE = (32, 32)
 BATCH_SIZE = 100
 NUM_EPOCHS = 10
 NUMBER_OF_LABELS = 3
@@ -74,7 +67,7 @@ class TinyVGG(nn.Module):
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(in_features=hidden_shape * 53 * 53, out_features=output_shape)  # image shape 32, 32
+            nn.Linear(in_features=hidden_shape * 5 * 5, out_features=output_shape)  # image shape 32, 32
         )
 
     def forward(self, x: torch.Tensor):
@@ -85,7 +78,7 @@ class TinyVGG(nn.Module):
 
 
 model = TinyVGG(input_shape=3, hidden_shape=10, output_shape=3)
-print(model(torch.randn(1, 3, 224, 224)))
+print(model(torch.randn(1, 3, 32, 32)))
 
 
 def train_step(
@@ -146,3 +139,89 @@ def test_step(
     test_loss = test_loss / len(dataloader)
     test_acc = test_acc / len(dataloader)
     return test_loss, test_acc
+
+
+def train(
+        model: torch.nn.Module,
+        train_dataloader: torch.utils.data.DataLoader,
+        test_dataloader: torch.utils.data.DataLoader,
+        optimizer: torch.optim.Optimizer,
+        loss_fn: torch.nn.Module,
+        epochs: int,
+        device: torch.device
+):
+    print(f"[INFO] Training model {model.__class__.__name__} on device '{device}' for {epochs} epochs...")
+
+    results = {
+        "train_loss": [],
+        "train_acc": [],
+        "test_loss": [],
+        "test_acc": []
+    }
+
+    for epoch in tqdm(range(epochs)):
+        test_loss, test_acc = test_step(model=model, dataloader=test_dataloader, loss_fn=loss_fn, device=device)
+        train_loss, train_acc = train_step(model=model, dataloader=train_dataloader, loss_fn=loss_fn, optimizer=optimizer, device=device)
+
+        print(
+            f"Epoch: {epoch + 1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+
+    return results
+
+
+torch.manual_seed(42)
+
+print(f"PyTorch version: {torch.__version__}")
+print(f"Is MPS (Metal Performance Shader) built? {torch.backends.mps.is_built()}")
+print(f"Is MPS available? {torch.backends.mps.is_available()}")
+
+device = "mps" if torch.backends.mps.is_available() else "cpu"
+print(f"Using device: {device}")
+
+model = TinyVGG(input_shape=3, hidden_shape=10, output_shape=3).to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001)
+
+from timeit import default_timer as timer
+
+start_time = timer()
+
+model_results = train(
+    model=model,
+    train_dataloader=train_loader,
+    test_dataloader=test_loader,
+    optimizer=optimizer,
+    loss_fn=loss_fn,
+    epochs=NUM_EPOCHS,
+    device=device
+)
+
+end_time = timer()
+
+total_train_time = print_train_time(start=start_time, end=end_time, device=device)
+
+results = {
+    "device": device,
+    "dataset_name": "CIFAR10",
+    "epochs": NUM_EPOCHS,
+    "batch_size": BATCH_SIZE,
+    "image_size": IMAGE_SIZE[0],
+    "num_train_samples": len(train_set),
+    "num_test_samples": len(test_set),
+    "total_train_time": round(total_train_time, 3),
+    "time_per_epoch": round(total_train_time / NUM_EPOCHS, 3),
+    "model": model.__class__.__name__,
+    "test_accuracy": model_results["test_acc"][-1]
+}
+print(results)
